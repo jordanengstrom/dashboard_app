5886cef8e5a56d54d152b91be43e1f57d71fc0d9
Jordan Engstrom <jordan.engstrom@gmail.com>
2021-01-19 23:40:41 -0700

    added logout route to urls.py

--- dashboard_app/users/urls.py
+++ dashboard_app/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import users_view, register, login, AuthenticatedUser
+from .views import users_view, register, login, logout,AuthenticatedUser
 
 
 urlpatterns = [
     path('users', users_view),
     path('register', register),
     path('login', login),
+    path('logout', logout),
     path('user', AuthenticatedUser.as_view()),
 ]
