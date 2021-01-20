bb763eb260dbd0832bbc405743e7b8a1f350e888
Jordan Engstrom <jordan.engstrom@gmail.com>
2021-01-20 00:28:55 -0700

    added concrete implementation of RoleViewSet.list method

--- dashboard_app/users/views.py
+++ dashboard_app/users/views.py
@@ -3,8 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from .models import User, Permission
-from .serializers import UserSerializer, PermissionSerializer
+from .models import User, Permission, Role
+from .serializers import UserSerializer, PermissionSerializer, RoleSerializer
 from .authentication import generate_access_token, JWTAuthentication
 
 
@@ -85,7 +85,10 @@ class RoleViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
 
     def list(self, request):
-        pass
+        serializer = RoleSerializer(Role.objects.all(), many=True)
+        return Response({
+            'data': serializer.data
+        })
 
     def retrieve(self, request, pk=None):
         pass
